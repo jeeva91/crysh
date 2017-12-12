b82ad766487106890a62152830bd81431427d0dc
jeeva <jee1199@gmail.com>
2017-12-12 02:44:26 -0500

    salt key and iv retrived

--- trydec.c
+++ trydec.c
@@ -14,22 +14,23 @@
 int
 main(void){
   int input, nr;
-  char* buff;
-  char* pass;
-  char salt[9];
+  unsigned char* buff;
+  unsigned char* pass;
+  unsigned char salt[PKCS5_SALT_LEN];
   unsigned char key[32];
-  unsigned char  iv[32];
+  unsigned char  iv[16];
   const EVP_CIPHER *cipher;
-  const EVP_MD *digest = NULL;
-  pass = getenv("PASS");
-  cipher = EVP_get_cipherbyname("aes-256-cbc");
+  const EVP_MD *digest;
+  pass = (unsigned char*)getenv("PASS");
+  printf("the password is %s\n", pass);
+  cipher = EVP_aes_256_cbc();
 
   if(cipher == NULL)
-    ErrorMessage("cipher failed");
+    perror("cipher failed");
 
-  digest = EVP_get_digestbyname("SHA1");
+  digest = EVP_sha1();
   if(digest == NULL)
-    ErrorMessage("digest failed");
+    perror("digest failed");
   
   if((buff = malloc(BUFFSIZE))==NULL){
     perror("malloc failed");
@@ -41,10 +42,36 @@ main(void){
   if((nr = read(input, buff, BUFFSIZE))<0){
     perror("unable to read the file");
   }
-  printf("read the following %s\n%d\n", buff, nr);
-  strncpy(salt, buff+8, 8);
-  printf("the salt is %s\n", salt);
-  EVP_BytesToKey(cipher, digest, salt, (unsigned char*)pass, strlen(pass), 1, key, iv)
+  close(input);
+  memcpy(salt, buff+8, PKCS5_SALT_LEN);
+
+  EVP_BytesToKey(cipher, digest, salt, pass, (int)strlen(pass), 1, key, iv);
+
+  //  printf("key = %s\niv = %s\n", key,iv);
+  // printf("the salt length is %d\n", PKCS5_SALT_LEN);
   
+  printf("salt = ");
+  printf("%02X", salt[0]);
+  printf("%02X", salt[1]);
+  printf("%02X", salt[2]);
+  printf("%02X", salt[3]);
+  printf("%02X", salt[4]);
+  printf("%02X", salt[5]);
+  printf("%02X", salt[6]);
+  printf("%02X", salt[7]);
+
+  printf("\nkey = ");
+  int i;
+  for(i=0;i<32;i++){
+    printf("%02X", key[i]);
+  }
+
+  printf("\niv = ");
+  
+  for(i=0;i<16;i++){
+    printf("%02X", iv[i]);
+  }
+
+  printf("\n");
   return 0;
 }

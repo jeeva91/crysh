1ccf1b119ba0799b8badb840d5cdcdd5df09e584
jeeva <jee1199@gmail.com>
2017-12-13 04:02:27 -0500

    strip sapce newline and tab added

--- getcommands.c
+++ getcommands.c
@@ -8,39 +8,58 @@
 
 int get_command(char*, size_t);
 int execute(char*);
+char* strstrip(char* token);
 
 int
 main(void){
-  char *lineptr = NULL;
+  char *commands, *lineptr = NULL;
   size_t linelen;
 
-
-  getline(&lineptr, &linelen, stdin);
-  if(linelen>0&&lineptr[linelen-1]=='\n'){
-    lineptr[--linelen]= '\0';
-    linelen-=1;
-  }
-  //  printf("the input is %s\n", lineptr);
-  get_command(lineptr, linelen);
-  //printf("after exec %s\n", lineptr);
-
-  
+  linelen = getline(&lineptr, &linelen, stdin);
+  commands = lineptr;
+  if((commands = strstrip(commands))!=NULL)
+    get_command(commands, linelen);
+  free(lineptr);
   return 0;
 }
-
+/* gets the independent commands and executes them recursively */
 int
 get_command(char* commands, size_t len){
   char* token;
+  
   if((commands==NULL)||(commands[0]=='\0'))
     return 0;
+
   token = strsep(&commands, ";");
-  //printf("%s\n", token);
-  if(token[0]!='\0')
-  execute(token);
+
+  if((token = strstrip(token))!=NULL)
+    execute(token);
+
   get_command(commands, len-strlen(token)-1);
   return 0;
 }
 
+/* function to strip out the space, tab and newline in front and back of string */
+char*
+strstrip(char* token){
+  size_t  tokenlen;
+  tokenlen = strlen(token);
+
+  while((token[0]==' ')||(token[0]=='\t')||(token[0]=='\n'))
+    token = token+1;
+
+  tokenlen = strlen(token);
+  while((token[tokenlen-1]=='\t')||(token[tokenlen-1]=='\n')||(token[tokenlen-1]==' ')){
+    token[tokenlen-1]='\0';
+    tokenlen-=1;
+  }
+  
+  if(token[0]=='\0')
+    token=NULL;
+  return token;
+}
+
+/* forks are executes the given command */
 int
 execute(char* command){
   if((command[0]=='\0')||command[0]=='\n')
